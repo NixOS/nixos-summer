e0cee2981c4055cf40b78450677d5ed191aaf5a9
Rok Garbas <rok@garbas.si>
2021-03-14 00:38:32 +0100

    initial skeleton

---
+++ scripts/serve.py
@@ -0,0 +1,43 @@
+import click
+import livereload
+import os
+
+
+@click.command()
+@click.option(
+    '--common-styles',
+    default=None,
+    type=click.Path(
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        readable=True,
+        resolve_path=True,
+    ),
+)
+def main(common_styles):
+    server = livereload.Server()
+
+    paths_to_watch = [
+        "./**/*.html",
+        "./*.html",
+        "./*.nix",
+        "./images/*",
+        "./js/*",
+        "./scripts/*",
+        "./styles/*.less",
+        "./styles/pages/*.less",
+    ]
+
+    if common_styles is not None:
+        paths_to_watch.append(common_styles)
+
+    for path in paths_to_watch:
+        server.watch(path, lambda: os.system("nix-build"))
+
+    os.system("nix-build")
+
+    server.serve(root="./result/", port=os.getenv("PORT", 8000))
+
+if __name__ == "__main__":
+    main()
